b8c49dca1970e3c01476859b59c7b4b2c02d34ac
Peter <113168584+prawstho@users.noreply.github.com>
2024-05-14 14:14:18 -0230

    write text to http

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello WeekThree!');
-});
+serveText('Hello World');
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+function serveText(theText){
+  const server = http.createServer(function (req, res) {
+    console.log('text was served.')
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(theText);
+    res.write(', eat more lunch');
+    res.end();
+  });
+
+    server.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  };
+
