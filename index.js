fe1233b819c5fac6d8034446606169e20ba8ba2e
Peter <113168584+prawstho@users.noreply.github.com>
2024-05-14 14:21:27 -0230

    displayed html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
-serveText('Hello World');
+// serveText('Hello World');
+serveHtml('<html><body><b>Woot</b> Woot</body></html>');
 
 function serveText(theText){
   const server = http.createServer(function (req, res) {
@@ -11,8 +12,20 @@ function serveText(theText){
     res.end();
   });
 
-    server.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-  };
+  server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+};
 
+function serveHtml(theHtml){
+  const server = http.createServer(function (req, res) {
+    console.log('html was served.')
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(theHtml);
+    res.end();
+  });
+
+  server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+}
