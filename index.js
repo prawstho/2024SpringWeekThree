a6eaad43cfc53fbec0722af6e1a9ed0c55c17495
Peter <113168584+prawstho@users.noreply.github.com>
2024-05-14 14:02:18 -0230

    working http server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello, WeekThree!');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello WeekThree!');
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
